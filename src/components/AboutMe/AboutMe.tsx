21090e306826031ba01ffa754c85a8f135d1e443
Gabrielmanuel11 <gabrielmanuel11@outlook.com>
2025-04-11 10:49:40 -0300

    Images fix 1.1

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -5,10 +5,10 @@ const AboutMe = () => {
   const { language } = useLanguage();
 
   const images = [
-    { src: 'public/assets/myimages/myfamily.jpeg', label: language === 'Português' ? 'Família' : 'Family' },
-    { src: 'public/assets/myimages/soccer.jpeg', label: language === 'Português' ?  'Futebol' : 'Soccer' },
-    { src: 'public/assets/myimages/book.jpeg', label: language === 'Português' ? 'Livro Favorito' : 'Favorite Book' },
-    { src: 'public/assets/myimages/superman.jpg', label: language === 'Português' ? 'Filme Favorito' : 'Favorite Movie' },
+    { src: '/assets/myimages/myfamily.jpeg', label: language === 'Português' ? 'Família' : 'Family' },
+    { src: '/assets/myimages/soccer.jpeg', label: language === 'Português' ?  'Futebol' : 'Soccer' },
+    { src: '/assets/myimages/book.jpeg', label: language === 'Português' ? 'Livro Favorito' : 'Favorite Book' },
+    { src: '/assets/myimages/superman.jpg', label: language === 'Português' ? 'Filme Favorito' : 'Favorite Movie' },
   ];
 
   return (
